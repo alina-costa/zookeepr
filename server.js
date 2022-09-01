9e7a0243dcfffd4f63fbb3f133f42d30f596cc94
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-09-01 13:06:10 -0400

    handle both GET and POST requests

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+// async await
+// synchronous vs asynchronous
+// ask about req, res
+// what is an API endpoint route?
+// GET vs POST
+// fetch ?
+// understanding the /:id route?
+// help with setting up / organizing the project
+
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-const app = express();
 const { animals } = require("./data/animals.json");
+const { json } = require("body-parser");
+const app = express();
+// parse incoming string or array data
+// takes incoming POST data and converts it to key/value pairings that can be accessed in the req.body object
+// informs our server that there may be sub-array data nested in it as well,
+// so it needs to look as deep into the POST data as possible to parse all of the data correctly.
+app.use(express.urlencoded({ extended: true }));
+// parse incoming json data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -14,7 +33,7 @@ function filterByQuery(query, animalsArray) {
     if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
-      personailityTraitsArray = query.personalityTraits;
+      personalityTraitsArray = query.personalityTraits;
     }
     // Loop through each trait in the personalityTraits array:
     personalityTraitsArray.forEach((trait) => {
@@ -55,6 +74,34 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  JSON.stringify({ animals: animalsArray }, null, 2);
+
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -69,6 +116,19 @@ app.get("/api/animals/:id", (req, res) => {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
   }
 });
 
